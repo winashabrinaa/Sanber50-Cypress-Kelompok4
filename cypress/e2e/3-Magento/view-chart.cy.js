32845c65ee53f0b6a1cd16203693f42800ef2db0
salsabila191 <sasabil191@gmail.com>
2023-10-23 16:37:17 +0700

    update cart

--- cypress/e2e/3-Magento/view-chart.cy.js
+++ cypress/e2e/3-Magento/view-chart.cy.js
@@ -1,12 +1,27 @@
-describe('View & Update Chart', () => {
-  it('View Chart', () => {
-    cy.visit('/')
-    cy.contains('My Cart').click();
-  })
 
-  it('View&chart Chart', () => {
-    cy.visit('/')
-    cy.get('[data-bind="i18n:"').contains('View and Edit Cart').click();
-    
-  })
-})
+  describe('View & Update Chart', () => {
+    it('View Chart', () => {
+      cy.visit('/');
+      cy.get('.minicart-wrapper').click();
+      cy.contains('My Cart').click();
+      cy.wait(3000);
+    });
+
+    it('Update Qty in Chart', () => { 
+      cy.get('.action.viewcart').submit();
+      cy.url().should('include','/checkout/cart/') // Show link to edit item
+      cy.get('input').clear().type('2'); // Change the quantity
+      cy.get('.update-cart-item').submit(); // Click the "Update" button
+      cy.get('.input-text.qty').should('have.value', '2'); // Verify the updated quantity
+      cy.get('.minicart-wrapper').click();
+    });
+
+    it('delete item in Chart', () => { 
+      cy.visit('https://magento.softwaretestingboard.com/checkout/cart/');
+      cy.get('a.action.action-delete').click();
+      cy.get('.cart-item').should('not.exist');
+      cy.get('.minicart-wrapper').click();
+
+    });
+
+  });
--- cypress/e2e/3-Magento/view-product.cy.js
+++ cypress/e2e/3-Magento/view-product.cy.js
@@ -4,17 +4,5 @@ describe('View Product', () => {
     cy.get('#ui-id-4').click()
     cy.get('span.base').should('contain', 'Women')
   })
-
-  /*it('dropdown Tops', () => {
-    cy.visit('/')
-    // Mengklik dropdown
-    cy.get('#ui-id-11').click();
-  
-    // Memilih opsi "Pilihan 2"
-    cy.get('#ui-id-11').select('Hoodies & Sweatshirts');
-  
-    // Memeriksa apakah opsi yang dipilih adalah "Pilihan 2"
-    cy.get('#ui-id-11').should('have.value', 'Hoodies & Sweatshirts');
-  }); */
-  
+ 
 })
